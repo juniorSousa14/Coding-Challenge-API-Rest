a7bb467427cac4fa634ff95943368b1cffea00af
Junior Sousa <jose.valberto@maisunifacisa.com.br>
2025-10-06 10:34:25 -0300

    first commit

---
+++ src/main/java/com/juniorjvsousa/Coding_Challenge_API_rest/CodingChallengeApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.juniorjvsousa.Coding_Challenge_API_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juniorjvsousa/Coding_Challenge_API_rest/CodingChallengeApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorjvsousa.Coding_Challenge_API_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
